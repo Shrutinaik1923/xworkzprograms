ef6bb6db770610f1f825c91efb023db31b459fef
shrutinaik29 <shrutinaik2019@gmail.com>
2023-07-19 22:49:47 +0530

    success

---
+++ Sports.java
@@ -0,0 +1,13 @@
+//
+class Sports{
+
+   public static void main(String[] args)
+   {
+   System.out.println("I love sports");
+   System.out.println("my fav game is volleyball");
+   System.out.println("national game is hockey");
+   System.out.println("gurinder singh is captain of indian volleyball team");
+   System.out.println("sports need physical effort and skills");
+   
+   }
+}
