23700196ff762d5b29ed043d8d7819feda8404f6
shrutinaik29 <shrutinaik2019@gmail.com>
2023-08-23 00:07:12 +0530

    aug22inbultclass

---
+++ aug21string/src/com/xworkz/stringInbuiltClass/ThreadRunner.java
@@ -0,0 +1,18 @@
+package com.xworkz.stringInbuiltClass;
+
+public class ThreadRunner {
+
+
+		public static void main(String[] args) {
+			Thread mainThread = Thread.currentThread();
+
+			System.out.println("Thread Name: " + mainThread.getName());
+			System.out.println("Thread ID: " + mainThread.getId());
+			System.out.println("Thread Priority: " + mainThread.getPriority());
+			System.out.println("Is Daemon Thread: " + mainThread.isDaemon());
+
+		}
+
+	}
+
+
